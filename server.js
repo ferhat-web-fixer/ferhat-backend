e57c5515ff0e1511d84fd9cee93c55a77e0ddcab
ferhat <erolferhat014@gmail.com>
2025-12-05 14:08:38 +0300

    fix port env

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const http = require('http');
 
 const hostname = '0.0.0.0';
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
