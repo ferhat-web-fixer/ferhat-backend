7c62c1968004783cc31c6a55f6634055554edc24
ferhat <erolferhat014@gmail.com>
2025-12-05 15:10:58 +0300

    Update API endpoint with timestamp

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
-if (req.url === '/') {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Merhaba Ferhat! Node container çalışıyor.\n');
-} else if (req.url === '/api') {   // buradaki parantez şart
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ message: 'API endpoint çalışıyor!' }));
-} else {
-  res.statusCode = 404;
-  res.end('Sayfa bulunamadı.');
-}
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Merhaba Ferhat! Node container çalışıyor.\n');
+  } else if (req.url === '/api') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ message: 'API endpoint çalışıyor!' }));
+  } else {
+    res.statusCode = 404;
+    res.end('Sayfa bulunamadı.');
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
